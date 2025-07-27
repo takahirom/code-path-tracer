a951e4b7b3ff717d1c3cf7ea8b1d09ef0d539ae0
takahirom <takam.dev@gmail.com>
2025-07-28 08:58:21 +0900

    Remove comments from build.gradle.kts

--- code-path-finder/build.gradle.kts
+++ code-path-finder/build.gradle.kts
@@ -3,13 +3,9 @@ plugins {
 }
 
 dependencies {
-  // Kotlin standard library
   implementation("org.jetbrains.kotlin:kotlin-stdlib")
-  // ByteBuddy for method tracing
   implementation("net.bytebuddy:byte-buddy:1.14.5")
   implementation("net.bytebuddy:byte-buddy-agent:1.14.5")
-  
-  // JUnit for testing and core functionality
   implementation("junit:junit:4.13.2")
 }
 
