bdfa7e433b661c8b373f9e46ba7a3b2393310d6d
takahirom <takam.dev@gmail.com>
2025-07-31 09:32:10 +0900

    Remove debug prints and improve thread safety

--- code-path-tracer/src/main/kotlin/io/github/takahirom/codepathtracer/DefaultFormatter.kt
+++ code-path-tracer/src/main/kotlin/io/github/takahirom/codepathtracer/DefaultFormatter.kt
@@ -4,6 +4,7 @@ package io.github.takahirom.codepathtracer
  * Default formatter for trace events with configurable max length.
  */
 object DefaultFormatter {
+    @Volatile
     var defaultMaxLength = 30
     
     fun format(event: TraceEvent): String = event.defaultFormat(defaultMaxLength)
